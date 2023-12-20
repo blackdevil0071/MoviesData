444a0e1382448205f670e31e991d23d844d0203a
suhasKumar <suhassuhas3468@gmail.com>
2023-12-20 11:03:31 +0530

    useState

--- Desktop/REACT-TUTORIALS1/my-first-app/src/components/Expenses/ExpenseItems.js
+++ Desktop/REACT-TUTORIALS1/my-first-app/src/components/Expenses/ExpenseItems.js
@@ -1,20 +1,29 @@
+import React ,{useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 
 export default function ExpenseItems(props) {
-  const deleteExpense = () => {
-    const expenseItem = document.querySelector('.expense-item')
-    expenseItem.parentNode.removeChild(expenseItem)
-    console.log("Item Deleted Succesfully")
-    
-  };
+  
+  const [amount,setAmount]=useState(props.amount)
+
+
+  // const [title,SetTitle]=useState(props.title)
+  //Update
+
+  console.log("Expense items Executed")
+  const clickHandler =() =>{
+    setAmount('100')
+    console.log(amount)
+    // SetTitle("Updated")
+    // console.log(title)
+  }
 
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <h2>{props.title}</h2>
-      <div className="expense-item__price">${props.amount}</div>
-      <button onClick={deleteExpense}>DeleteExpenses</button>
+      <div className="expense-item__price">${amount}</div>
+      <button onClick={clickHandler}>changeTitle</button>
     </div>
   );
 }
