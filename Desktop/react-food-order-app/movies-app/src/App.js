876f261c4fe407ee461a70c7f51a5430bdfdc4f2
suhasKumar <suhassuhas3468@gmail.com>
2024-01-08 16:17:58 +0530

    ERROR HANDLING IN GET API

--- Desktop/react-food-order-app/movies-app/src/App.js
+++ Desktop/react-food-order-app/movies-app/src/App.js
@@ -1,39 +1,72 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./Movies";
-import { Spinner } from "react-bootstrap";
+import { Spinner, Button } from "react-bootstrap";
 import React, { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [retrying, setRetrying] = useState(false);
 
   async function fetchMoviesHandler() {
+    setError(false);
     setIsLoading(true);
-    console.log("Button Clicked");
-    try {
-      const response = await fetch("https://swapi.dev/api/films");
-      const data = await response.json();
+    setRetrying(true); // Start retrying
 
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong....Retrying");
+      }
+      const data = await response.json();
       setMovies(data.results);
+      setRetrying(false); // Stop retrying on successful fetch
     } catch (error) {
-      console.log("ERROR", error);
+      console.log("ERROR", error.message);
+      setError(error.message);
+
+      // Retry the fetch after 5 seconds if retrying is true
+      if (retrying) {
+        setTimeout(() => {
+          fetchMoviesHandler();
+        }, 5000);
+      }
     } finally {
       setIsLoading(false);
     }
   }
 
+  const handleCancelRetry = () => {
+    setRetrying(false);
+  };
+
+  let content = <p>No Movies Found</p>;
+  if (isLoading) {
+    content = (
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  }
+  if (movies.length > 0) {
+    content = <Movies movies={movies} />;
+  }
+  if (error) {
+    content = (
+      <div>
+        <p>{error}</p>
+        {retrying && <Button onClick={handleCancelRetry}>Cancel Retry</Button>}
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <button onClick={fetchMoviesHandler}>Fetch Movies</button><pre></pre>
-      {!isLoading && movies.length > 0 && <Movies movies={movies} />}
-      {isLoading && (
-        <Spinner animation="border" role="status">
-          <span  className="visually-hidden">Loading...</span>
-        </Spinner>
-      )}
-      {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
-      <div></div>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      <pre></pre>
+      {content} <div></div>
     </div>
   );
 }
