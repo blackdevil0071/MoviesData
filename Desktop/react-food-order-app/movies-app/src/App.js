02b0b02ab5c6b5ed43978e5a7bc3abf4da6e0449
suhasKumar <suhassuhas3468@gmail.com>
2024-01-08 13:05:47 +0530

    LOADING SPINNER TO FETCH API

--- Desktop/react-food-order-app/movies-app/src/App.js
+++ Desktop/react-food-order-app/movies-app/src/App.js
@@ -1,26 +1,39 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Movies from "./Movies";
-import MoviesList from "./MoviesList";
+import { Spinner } from "react-bootstrap";
 import React, { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+    console.log("Button Clicked");
     try {
-      const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch("https://swapi.dev/api/films");
       const data = await response.json();
-      setMovies(data.results);  // Just set data directly
+
+      setMovies(data.results);
     } catch (error) {
       console.log("ERROR", error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
   return (
     <div className="App">
-      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      <Movies movies={movies} />
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button><pre></pre>
+      {!isLoading && movies.length > 0 && <Movies movies={movies} />}
+      {isLoading && (
+        <Spinner animation="border" role="status">
+          <span  className="visually-hidden">Loading...</span>
+        </Spinner>
+      )}
+      {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
+      <div></div>
     </div>
   );
 }
